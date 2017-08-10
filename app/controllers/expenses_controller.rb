2fe7b431b4b08986bb3ba3cbe3b33ff6c680f403
Mauricio Lancheros <lucalan66@gmail.com>
2017-08-10 13:03:28 -0500

    or

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,12 @@
 class ExpensesController < ApplicationController
   def index
+  	if params[:concept] || params[:category_id]
+  		search = "%" + params[:concept] + "%"
+  		@expenses = Expense.where("concept LIKE ? OR category_id LIKE ?", search, params[:category_id])
+
+
+  	else
      @expenses = Expense.order("date DESC")
+ 	end
   end
 end
